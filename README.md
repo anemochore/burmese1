3e04ae431daafe059f271bf63f1e692e18fc7413
cuttingMoments <CuttingMoments@gmail.com>
2016-01-25 22:53:03 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# burmese typo 1
+
+2016-1-24~25
+javascript canvas only.
+text from https://my.wikipedia.org/wiki/မြန်မာဘာသာ
+using burmese webfont 'padauk' from http://mmwebfonts.comquas.com
---
+++ scripts/main.js
@@ -0,0 +1,179 @@
+/**
+ * Created by anemochore on 2016-01-24.
+ */
+'use strict';
+
+const TXT = 'မူလအစ ဗြာဟ္မီက မြန်မာစာ အရေးအသား ပေါ်ပေါက်ဖြစ်ထွန်းပုံကို စူးစမ်းလေ့လာခဲ့ကြသော သုတေသီအားလုံးကပင် မြန်မာအရေးအသား၊ မြန်မာအက္ခရာသည် ခရစ်တော်မပေါ်မီ ဘီစီ ၅၀၀ ခန့်မှ အေဒီ ၃၀၀ ကျော်အထိ အိန္ဒိယဒေသတွင် ထွန်းကားခဲ့သည့် ဗြာဟ္မီအရေးအသား၌ မြစ်ဖျားခံသည်ဟု လက်ခံကြသည်။ မြန်မာအရေးအသားစနစ်သည် ဗြာဟ္မီအရေးအသားစနစ်ကဲ့သို့ပင် ဗျည်းအက္ခရာများ၊ ဗျည်းတွဲသင်္ကေတများ၊ သရအက္ခရာများ၊ သရသင်္ကေတများ၊ နိဂ္ဂဟိတ် (သေးသေးတင်)၊ ဝိသဇ္ဇနီ (ဝစ္စပေါက်) သင်္ကေတများဟူသော ပစ္စည်းအဆောက်အဦများဖြင့် တည်ဆောက်သည့် အရေးအသားစနစ်ဖြစ်သည်။ ဗြာဟ္မီအရေးအသားကို အသုံးပြုသည့် အခြားစကားများနှင့် မြန်မာစကားတို့၏ ခြားနားမှုများကြောင့် ဗျည်းအရေအတွက်၊ သရအရေအတွက်၊ သင်္ကေတအမျိုးအမည်နှင့် အရေအတွက် အနည်းငယ်ကွာခြားမှု ရှိစေကာမူ မြန်မာအရေးအသားစနစ်သည် ဗြာဟ္မီအရေးအသားစနစ်နှင့် အခြေခံအားဖြင့် တူညီသည်။ ဗြာဟ္မီဗျည်းအက္ခရာ၊ ဗျည်းတွဲသင်္ကေတ၊ သရအက္ခရာ၊ သရသင်္ကေတ၊ နိဂ္ဂဟိတ်၊ ဝိသဇ္ဇနီတို့၏ ပုံသဏ္ဌာန်များ ခေတ်အဆက်ဆက် ပြောင်းလဲပုံကို ခြေရာခံကြည့်သည့်အခါတွင်လည်း မြန်မာဗျည်းအက္ခရာ၊ ဗျည်းတွဲသင်္ကေတ၊ သရအက္ခရာ၊ သရသင်္ကေတ၊ နိဂ္ဂဟိတ်၊ ဝိသဇ္ဇနီတို့၏ ပုံသဏ္ဌာန်များနှင့် ဆက်နွှယ်နေသည်ကို အထင်အရှား တွေ့နိုင်ပေသည်။ ယင်းသို့ ပစ္စည်းအဆောက်အဦး အားဖြင့်လည်းကောင်း၊ အက္ခရာပုံသဏ္ဌာန်အားဖြင့်လည်းကောင်း တူညီဆက်နွှယ်မှုရှိခြင်းကြောင့် မြန်မာအရေးအသားသည် ဗြာဟ္မီအရေးအသား၌ မြစ်ဖျားခံသည်ဟူသော အချက်မှာ အငြင်းပွားဖွယ် မရှိပေ။ ဗြာဟ္မီမှ ဆင့်ကဲဖြစ်ထွန်းလာသော အရေးအသားများ ဗြာဟ္မီအရေးအသားကို ဘီစီ ၅ ရာစုခန့်က စတင်တွေ့ရသည်။ ၎င်းသည် ဘီစီ ၃ ရာစုအတွင်း အသောကမင်းလက်ထက်တွင် အိန္ဒိယတစ်ခွင်တစ်ပြင်၌ ပျံ့နှံ့ထွန်းကားခဲ့သည်။ အသောကမင်းတည်ထောင်သော မောရိယနိုင်ငံကြီးပျက်စီးပြီးနောက် အိန္ဒိယမြောက်ပိုင်းတွင် ကုသျှာနမင်းဆက်၊ ထို့နောက် ဂုပ္တမင်းဆက်တို့ ပေါ်ပေါက်ကြီးစိုးခဲ့သည်။ ယင်းမင်းများလက်ထက် ဗြာဟ္မီအက္ခရာသည် တစ်စတစ်စ ပုံသဏ္ဌာန်ပြောင်းလဲလာသည်။ မင်းများကိုအစွဲပြု၍ ယင်းတို့လက်ထက် အရေးအသားများကို ကုသျှာနအရေးအသား၊ ဂုပ္တအရေးအသားဟု ခေါ်တွင်ခဲ့ကြသည်။ ထိုမှတဖန် အကောက်အကွေ့များသော ကုဋိလအရေးအသား၊ နာဂစာ၊ မြို့သုံးစာဟုဆိုကြသော နာဂရီအရေးအသေား၊ ရှင်းလင်းသော သျှရဒါအရေးအသား၊ ဘင်္ဂလာဒေသတစ်ခွင်တွင်သုံးသော ဘင်္ဂါလီအရေးအသားများ ဒေသအလိုက်၊ ခေတ်အလိုက် ဆင့်ပွားဖြစ်ထွန်းလာသည်။ ယင်းအရေးအသား အက္ခရာတို့သည် ကာလဒေသအလိုက် ပုံသဏ္ဌာန်အားဖြင့် တစ်စတစ်စ ခြားနားကွဲပြား လာခဲ့ကြသည်။ အိန္ဒိယတောင်ပိုင်းတွင်မူ မောရိယနိုင်ငံကြီး ပျက်စီးပြီးနောက် အန္ဓြမင်းဆက် ပေါ်ပေါက်ခဲ့သည်။ ထို့နောက် ပလ္လဝမင်းဆက်။ ကဓမ္ဗမင်းဆက်၊ စလုကျမင်းဆက်၊ ရဋ္ဌကူဋမင်းဆက်၊ စောဠမင်းဆက် စသည်တို့ ပေါ်ပေါက်ခဲ့သည်။ ယင်းမင်းများအုပ်စိုးရာ ခေတ်တစ်လျှောက်၊ အနောက်ဘက်ဒေသတွင် ထွန်းကားသည့် ပစ္ဆိမီအရေးအသား၊ အလယ်ပိုင်းဒေသတွင် ထွန်းကားသည့် မဓျပြဒေသျှီအရေးအသား၊ တောင်ဘက်ပိုင်းတွင် ထွန်းကားသော ကဓမ္ဗ၊ စလုကျ၊ ရဋ္ဌကူဋစသည်တို့ အကျုံးဝင်သည့် တေလုဂူဒေသ၊ ကနဍီဒေသတို့တွင်သုံးသော တေလုဂူ ကနဍီအရးအသား၊ကျမ်းဂန်သုံး ဂြန္ထအရေးအသား၊ တမီလအရပ်ဒေသသုံး တမီလအရေးအသား စသည့်အရေးအသားများ ဗြာဟ္မီမှ ဆင့်ပွားဖြစ်ထွန်း ပေါ်ပေါက်ခဲ့သည်။ ဗြာဟ္မီမှ ဆင်းသက်ဖြစ်ထွန်းလာသော ယင်းအိန္ဒိယ အရေးအသား အက္ခရာတို့သည် အေဒီ ၁ ရာစုမှ ၈ ရာစုအတွင်း တိဗက်၊ သီရိလင်္ကာ၊ မြန်မာ၊ ထိုင်း၊ ကမ္ဗောဒီးယား၊ အင်ဒိုနီးရှားစသည့် ဒေသများသို့ အိန္ဒိယအယူဝါဒ၊ ယဉ်ကျေးမှုများနှင့်အတူ ပျံ့နှံ့ရောက်ရှိပြီး ယင်းဒေသများတွင် တိုင်းရင်း အရေးအသားအက္ခရာများ ဖြစ်ထွန်းမှုအတွက် အထောက်အပံ့ပြုခဲ့ပေသည်။';
+
+var isPaused = false;
+
+// canvas
+const C_WIDTH = 640;
+const C_HEIGHT = 480;
+var can = document.createElement('canvas');
+var ctx = can.getContext('2d');
+var px, py;
+can.width = C_WIDTH ;
+can.height = C_HEIGHT;
+document.body.appendChild(can);
+can.addEventListener('mousemove', mouseMove);
+can.addEventListener('mousedown', mouseDown);
+
+// multiplier
+const SPR_ALPHA_M = 0.925;	// must be lesser than 1
+const SPR_FONT_SIZE_M = 1.5;
+
+// txtSprites
+const FONT_NAME = 'Padauk';
+const FONT_ALPHA_DEFAULT = 0.2;
+const FONT_SIZE_START = 12;
+const FONT_HEIGHT = FONT_SIZE_START * 1.4 >> 0;	// 1.4 may vary depending on font
+const LINE_SPACE = FONT_SIZE_START / 2 >> 0;
+var spr = [];
+
+//dev env
+//var _showRect = false;
+
+
+go();
+
+
+function go() {
+	ctx.textBaseline = 'top';
+
+	spr = txt2Sprites(TXT);
+
+	// starting loop
+	loop();
+}
+
+function loop() {
+	requestAnimationFrame(loop);
+
+	if(isPaused) return;
+
+	ctx.clearRect(0,0, C_WIDTH,C_HEIGHT);
+
+	for (var i = 0; i < spr.length; i++) {
+		ctx.fillStyle = rgba(0, 0, 0, FONT_ALPHA_DEFAULT);
+		if(spr[i].isAnimating) {
+			spr[i].alpha *= SPR_ALPHA_M;
+			if(spr[i].alpha < 0.1) {
+				spr[i].isAnimating = false;
+				spr[i].alpha = 1.0;
+
+				spr[i].fontSize = FONT_SIZE_START;
+				spr[i].width = spr[i].oWidth;
+				spr[i].height = spr[i].oHeight;
+				spr[i].x1 = spr[i].oX;
+				spr[i].y1 = spr[i].oY;
+			}
+			else {
+				spr[i].fontSize *= SPR_FONT_SIZE_M;
+				var deltaX = spr[i].width * SPR_FONT_SIZE_M - spr[i].width;
+				var deltaY = spr[i].height * SPR_FONT_SIZE_M - spr[i].height;
+				spr[i].width *= SPR_FONT_SIZE_M;
+				spr[i].height *= SPR_FONT_SIZE_M;
+				spr[i].x1 -= deltaX / 2;
+				spr[i].y1 -= deltaY / 2;
+			}
+			ctx.fillStyle = rgba(spr[i].r, spr[i].g, spr[i].b, spr[i].alpha);
+		}
+		ctx.font = spr[i].fontSize + 'px ' + FONT_NAME;
+		ctx.fillText(spr[i].word, spr[i].x1,spr[i].y1);
+
+		//dev env
+		/*
+		if(_showRect) {
+			ctx.strokeStyle = spr[i].color;
+			ctx.strokeRect(spr[i].x1,spr[i].y1, spr[i].width,FONT_HEIGHT);
+		}
+		*/
+	}
+}
+
+function txt2Sprites(text) {
+	ctx.font = FONT_SIZE_START + 'px ' + FONT_NAME;
+
+	var words = text.split(' ');
+	var x0 = 0, y0 = FONT_HEIGHT / 2;
+	var x = x0, y = y0;
+	var line = '';
+
+	var spr = [];
+	var spaceWidth = (ctx.measureText(' ')).width;
+
+	for (var i = 0; i < words.length; i++) {
+		var testLine = line + words[i];
+		var wordWidth = (ctx.measureText(words[i])).width;
+		var testWidth = (ctx.measureText(testLine)).width;
+
+		if (testWidth > C_WIDTH) {
+			line = words[i] + ' ';
+
+			y += FONT_HEIGHT + LINE_SPACE;
+			if(y > C_HEIGHT - FONT_HEIGHT) {
+				break;
+			}
+			x = x0;
+		} else {
+			line = testLine + ' ';
+		}
+
+		spr.push({
+			word: words[i],
+			oX: x,
+			oY: y,
+			oWidth: wordWidth,
+			oHeight: FONT_SIZE_START,
+			x1: x,
+			y1: y,
+			x2: x + wordWidth - 1,
+			y2: y + FONT_HEIGHT - 1,
+			width: wordWidth,
+			height: FONT_SIZE_START,
+			fontSize: FONT_SIZE_START,
+			alpha: FONT_ALPHA_DEFAULT,
+			r: Math.random() * 255 >> 0,
+			g: Math.random() * 255 >> 0,
+			b: Math.random() * 255 >> 0,
+			isAnimating: false
+		});
+
+		x += wordWidth + spaceWidth - 1;
+	}
+
+	return spr;
+}
+
+function mouseMove(e) {
+	var x = e.clientX - (can.offsetLeft - window.pageXOffset);
+	var y = e.clientY - (can.offsetTop - window.pageYOffset);
+
+	if((px === x && py === y) || isPaused) return;
+
+	// I really miss actionScript here...
+	for (var i = 0; i < spr.length; i++) {
+		if( x >= spr[i].x1 && x <= spr[i].x2 &&
+				y >= spr[i].y1 && y <= spr[i].y2) {
+			spr[i].isAnimating = true;
+			spr[i].alpha = 1.0;
+			px = x;
+			py = y;
+			break;
+		}
+	}
+}
+
+function rgba(r, g, b, a) {
+	// r,g,b is [0, 255)
+	// a is [0, 1)
+	return 'rgba(' + String(r>>0) + ',' + String(g>>0) + ',' + String(b>>0) + ',' + a + ')';
+}
+
+function mouseDown(e) {
+	isPaused = !isPaused;
+
+	//dev env
+	//_showRect = !_showRect;
+}
